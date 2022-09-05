f0a057648523ac89f8818863b87744c349ce87e0
raylu15742cs <raylucs15742@gmail.com>
2022-09-05 13:38:07 -0700

    initial test for app

--- testing-app/src/App.test.js
+++ testing-app/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("App Component", () => {
+  it("renders correct heading", () => {
+    const { getByRole} = render(<App />);
+    expect(getByRole("heading").textContent).toMatch(/our first test/i)
+  })
+})
