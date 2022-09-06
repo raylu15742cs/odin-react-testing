6f3e4048d956767fa69bb9af77f985fcf9f64f0b
raylu15742cs <raylucs15742@gmail.com>
2022-09-06 09:32:30 -0700

    update app.test.js for button

--- testing-app/src/App.js
+++ testing-app/src/App.js
@@ -1,20 +1,22 @@
-import React , {useState} from "react";
+// App.js
+
+import React, { useState } from 'react';
 
 const App = () => {
-    const [heading , setHeading] = useState("Magnificent Monkeys");
+  const [heading, setHeading] = useState('Magnificent Monkeys');
 
-    const clickHandler = () => {
-        setHeading( "Radical Rhinos" )
-    };
+  const clickHandler = () => {
+    setHeading('Radical Rhinos');
+  };
 
-    return (
-        <>
-            <button type="button" onClick={clickHandler}>
-                Click Me 
-            </button>
-            <h1>{heading}</h1>
-        </>
-    )
-}
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+};
 
 export default App;
--- testing-app/src/App.test.js
+++ testing-app/src/App.test.js
@@ -1,10 +1,23 @@
-import React from "react";
-import { render , screen } from '@testing-library/react';
+// App.test.js
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-describe("App Component", () => {
-  it("renders correct heading", () => {
-    const { getByRole} = render(<App />);
-    expect(getByRole("heading").textContent).toMatch(/our first test/i)
-  })
-})
+describe('App component', () => {
+  it('renders magnificent monkeys', () => {
+    // since screen does not have the container property, we'll destructure render to obtain container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders radical rhinos after button click', () => {
+    render(<App />);
+    const button = screen.getByRole('button', { name: 'Click Me' });
+
+    userEvent.click(button);
+
+    expect(screen.getByRole('heading').textContent).toMatch(/radical rhinos/i);
+  });
+});
